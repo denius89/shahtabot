1406cb6cc5a590aa8da1536ef79d7e39879a8528
denius89 <denisfedko@gmail.com>
2018-05-28 12:57:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const TOKEN = '602662764:AAF-cFBdYF2lEWR5HKdMdu66zqYmKI2Rf68'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg =>{
-    bot.sendMessage(msg.chat.id, 'Hello from Heroku, bot says: "Hi, ${msg.from.first_name}"')
+    bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says: "Hi, ${msg.form.first_name}"` )
     })
+
